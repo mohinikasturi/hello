59d7ca810611cd07fb6abad8b40f56d022dd4d3f
nikita <patilnikitac@gmail.com>
2018-12-04 07:58:26 -0500

    First commit

---
+++ src/main/java/com/scp/java/hinbernate/GitProject/App.java
@@ -0,0 +1,13 @@
+package com.scp.java.hinbernate.GitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
